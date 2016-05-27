8015f7498e2ad6b54707cc83a5d827608bf3b3cb
Hong-She Liang <starofrainnight@gmail.com>
2016-05-27 09:47:14 +0800

    Fixed not working in unix systems

--- ReText/singleapplication.py
+++ ReText/singleapplication.py
@@ -19,6 +19,7 @@ import struct
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import QObject
 from PyQt5.QtCore import QSharedMemory
+from PyQt5.QtCore import QSystemSemaphore
 from PyQt5.QtNetwork import QLocalServer
 from PyQt5.QtNetwork import QLocalSocket
 
@@ -45,10 +46,24 @@ class SingleApplication(QObject):
 		QObject.__init__(self, parent)
 		self._name = name
 		self._mode = self.Server
-		self._sharedMemory = None
+		self._sharedMemory = QSharedMemory(self._name)
+		self._systemSemaphore = QSystemSemaphore(self._name + "-2", 1)
 		self._server = None
 		self._client = None
 		self._localSockets = {}
+
+		# Fix for *nix:
+		# http://stackoverflow.com/questions/5006547/qt-best-practice-for-a-single-instance-app-protection
+		# http://habrahabr.ru/post/173281/
+
+		self._systemSemaphore.acquire();
+		try:
+			unixFix = QSharedMemory(self._name)
+			unixFix.attach()
+			unixFix.detach()
+			unixFix = None
+		finally:
+			self._systemSemaphore.release();
 
 	@property
 	def name(self):
@@ -65,14 +80,14 @@ class SingleApplication(QObject):
 		self._localSockets[localSocket] += localSocket.readAll()
 		data = self._localSockets[localSocket]
 		if len(data) > 4:
-			# First 4bytes is an native Long.
-			dataSize = struct.unpack("@L", data[:4])[0]
+			# First 4bytes is an native Integer.
+			dataSize = struct.unpack("@I", data[:4])[0]
 			receivedDataSize = len(data) - 4
 			if receivedDataSize < dataSize:
 				return
 
 			self.receivedMessage.emit(bytes(data[4:]))
-			
+
 			# Remove the command socket
 			del self._localSockets[localSocket]
 			localSocket.deleteLater()
@@ -86,8 +101,39 @@ class SingleApplication(QObject):
 
 	def start(self):
 		# Ensure we run only one application
-		self._sharedMemory = QSharedMemory(self._name)
-		if self._sharedMemory.create(1, QSharedMemory.ReadWrite):
+		isAnotherRunning = False
+		if not self._sharedMemory.isAttached():
+			# If share memory is attachable, that means there have another 
+			# running application
+			self._systemSemaphore.acquire()
+			try:
+				isAnotherRunning = self._sharedMemory.attach()
+				if isAnotherRunning:
+					self._sharedMemory.detach()
+			finally:
+				self._systemSemaphore.release()
+
+		if not isAnotherRunning:
+			# If we already attached by previous attach test, we will create 
+			# the share memory.
+			self._systemSemaphore.acquire()
+			try:
+				isAnotherRunning = not self._sharedMemory.create(1, QSharedMemory.ReadWrite)
+			finally:
+				self._systemSemaphore.release()
+
+			# If there something happened during create procedure lead us can't
+			# create one, we should detach that share memory.
+			if isAnotherRunning:
+				self._systemSemaphore.acquire()
+				try:
+					if self._sharedMemory.isAttached():
+						self._sharedMemory.detach()
+				finally:
+					self._systemSemaphore.release();
+
+		# Fine, now we could take different action on different situation. 
+		if not isAnotherRunning:
 			self._mode = self.Server
 			self._server = QLocalServer(self)
 			self._server.newConnection.connect(self._onServerNewConnection)
@@ -105,12 +151,10 @@ class SingleApplication(QObject):
 			# Detach immediately if create failed.
 			#
 			# WARNING: On windows os, seems if we don't detach the share memory
-			# after failed to create, another ReText can't create the share 
-			# memory after original instance exitted while there still have 
-			# ReText as client. 
-			self._sharedMemory.detach()
-			self._sharedMemory = None
-			
+			# after failed to create, another ReText can't create the share
+			# memory after original instance exitted while there still have
+			# ReText as client.
+
 			self._mode = self.Client
 			self._client = QLocalSocket(self)
 			self._client.connectToServer(self._name)
@@ -118,6 +162,6 @@ class SingleApplication(QObject):
 	def sendMessage(self, message):
 		# Only accept bytes message
 		assert(type(message) == bytes)
-		data = struct.pack("@L%ss" % len(message), len(message), message)
+		data = struct.pack("@I%ss" % len(message), len(message), message)
 		self._client.write(data)
-	
+
